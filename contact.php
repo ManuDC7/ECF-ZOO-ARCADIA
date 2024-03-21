ffa0543751d6b84347dfa5bd8cd7ff80b53a6947
Manuel De Carvalho <dctmp@icloud.com>
2024-03-21 08:06:13 +0100

    fix: dernier commit sur mongo et test email

--- contact.php
+++ contact.php
@@ -6,7 +6,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $message = $_POST["message"];
 
     $to = "secog38972@irnini.com"; 
-    $subject = "Nouveau message de $email: $title";
+    $subject = "$title";
     $headers = "From: $email";
 
     if (mail($to, $subject, $message, $headers)) {
